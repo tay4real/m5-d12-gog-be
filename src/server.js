7a190d1a1755bced0d563a0c03a7e23c783507b6
Otubusin Ademuyiwa <53128146+tay4real@users.noreply.github.com>
2020-12-28 22:38:47 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,33 +1,60 @@
-const express = require("express")
-require("dotenv").config()
-const listEndpoints = require("express-list-endpoints")
+const express = require("express");
+const cors = require("cors");
+const listEndpoints = require("express-list-endpoints");
+
+const gamesRouter = require("./services/games");
+const cartRouter = require("./services/cart");
 const {
-    notFoundHandler,
-    unauthorizedHandler,
-    forbiddenHandler,
-    badRequestHandler,
-    catchAllHandler,
-  } = require("./errorHandling")
+  notFoundHandler,
+  unauthorizedHandler,
+  forbiddenHandler,
+  badRequestHandler,
+  catchAllHandler,
+} = require("./errorHandling");
 
-const gamesRouter = require("./services/games")
-const cartRouter = require("./services/cart")
+const server = express();
 
-const server = express()
+const port = process.env.PORT || 3001;
 
-server.use(express.json())
+server.use(express.json());
 
-server.get("/", (req, res, next) => res.send("Server is running..."))
-server.use("/games", gamesRouter)
-server.use("/cart", cartRouter)
+const whiteList =
+  process.env.NODE_ENV === "production"
+    ? [process.env.FE_URL_PROD]
+    : [process.env.FE_URL_DEV];
 
-const port = process.env.PORT || 3001
+// const whiteList = [process.env.FE_URL_PROD, process.env.FE_URL_DEV];
 
-server.use(notFoundHandler)
-server.use(unauthorizedHandler)
-server.use(forbiddenHandler)
-server.use(badRequestHandler)
-server.use(catchAllHandler)
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) !== -1) {
+      // allowed
+      callback(null, true);
+    } else {
+      // Not allowed
+      callback(new Error("NOT ALLOWED - CORS ISSUES"));
+    }
+  },
+};
 
-console.log(listEndpoints(server))
+server.use(cors(corsOptions)); // CROSS ORIGIN RESOURCE SHARING
 
-server.listen(port, ()=> console.log("Server is running on port: " + port))
+server.get("/", (req, res, next) => res.send("Server is running..."));
+server.use("/games", gamesRouter);
+server.use("/cart", cartRouter);
+
+server.use(notFoundHandler);
+server.use(unauthorizedHandler);
+server.use(forbiddenHandler);
+server.use(badRequestHandler);
+server.use(catchAllHandler);
+
+console.log(listEndpoints(server));
+
+server.listen(port, () => {
+  if (process.env.NODE_ENV === "production") {
+    console.log("Server is running on cloud on port", port);
+  } else {
+    console.log("Server is running locally on port", port);
+  }
+});
