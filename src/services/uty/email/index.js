7b83b5a154ea8e15f38a3df07a5e74721a2c3e6b
Otubusin Ademuyiwa <53128146+tay4real@users.noreply.github.com>
2020-12-29 17:27:39 +0100

    Update index.js

--- src/services/uty/email/index.js
+++ src/services/uty/email/index.js
@@ -1,20 +1,39 @@
-const emailjs = require("emailjs-com");
-const uniqid = require("uniqid");
+const nodemailer = require("nodemailer");
 
 module.exports = async (emailAddress, attachment) => {
   try {
-    const template_id = uniqid();
-    const template_params = {
-      subject: "Testing Email JS",
-      text: "I hope this works",
+    const myFile64 = attachment.toString("base64");
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: process.env.SMTP_SERVER,
+      port: process.env.SMTP_PORT,
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+
+    // send mail with defined transport object
+    const mailOptions = {
+      from: `Ademuyiwa Otubusin <${process.env.SMTP_USER}>`, // sender address
+      to: emailAddress, // list of receivers
+      subject: "Testing Nodemailer", // Subject line
+      html: "<p>Your html here</p>", // plain text body
+      attachment: [
+        {
+          // encoded string as an attachment
+          filename: "whatever.pdf",
+          content: myFile64,
+          encoding: "base64",
+
+          contentDisposition: "attachment",
+        },
+      ],
     };
 
-    await emailjs.send(
-      process.env.SERVICE_ID,
-      template_id,
-      template_params,
-      process.env.USER_ID
-    );
+    await transporter.sendMail(mailOptions);
   } catch (error) {
     console.log(JSON.stringify(error));
   }
